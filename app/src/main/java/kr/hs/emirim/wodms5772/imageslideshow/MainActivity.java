31df0db8ec61c4c8f52e393bd21ea2d27f70da4b
Jaeeun Jung <wodms5772@naver.com>
2016-05-26 20:52:42 +0900

    #3 select time

--- app/src/main/java/kr/hs/emirim/wodms5772/imageslideshow/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wodms5772/imageslideshow/MainActivity.java
@@ -1,13 +1,44 @@
 package kr.hs.emirim.wodms5772.imageslideshow;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    ViewFlipper flip;
+    EditText edit;
+    int time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit=(EditText)findViewById(R.id.edit_text);
+        flip = (ViewFlipper)findViewById(R.id.view_flip);
+        Button butStart=(Button)findViewById(R.id.but_start);
+        butStart.setOnClickListener(this);
+        Button butStop=(Button)findViewById(R.id.but_stop);
+        butStop.setOnClickListener(this);
+
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.but_start:
+                time = (int)Double.parseDouble(edit.getText().toString())*1000;
+                flip.setFlipInterval(time);
+                flip.startFlipping(); break;
+            case R.id.but_stop:
+                flip.stopFlipping(); break;
+        }
     }
 }
